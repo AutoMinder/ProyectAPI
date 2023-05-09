301b9f32ebdcc8b2e9c4a1a7d1ebd314a1054c27
fernando00124221 <00124221@uca.edu.sv>
2023-05-09 12:03:22 -0600

    :art: Naming refactoring in Routes files in progress

--- AUTOMINDER_APP_API/routes/api/index.router.js
+++ AUTOMINDER_APP_API/routes/api/index.router.js
@@ -1,18 +1,15 @@
+//  LIBRARY IMPORTATION
 const express = require("express");
 
 const router = express.Router();
 
-
-//importando todos los enrutas de la carpeta api
-
-const postRouter = require("./post.router");
-
-const authRouter = require("./auth.router");
+//  Importando todos los enrutadores secundarios de la carpeta API
+const carRouter = require("./car.router");
+const ownerRouter = require("./owner.router");
 
 //Definir las rutas
+router.use("/car", carRouter);
+router.use("/owner", ownerRouter);
 
-router.use("/auth", authRouter);
-router.use("/post", postRouter);
-
-
+//  MODULE EXPORTATION
 module.exports = router;
