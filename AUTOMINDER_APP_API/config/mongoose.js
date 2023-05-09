6083aa51d79c34ac85e7c04bb70f0e2f5e443d5c
fernando <00124221@uca.edu.sv>
2023-05-08 18:54:27 -0600

    ADD: Deprecation warning fixed in Mongoose connection

--- AUTOMINDER_APP_API/config/mongoose.js
+++ AUTOMINDER_APP_API/config/mongoose.js
@@ -5,6 +5,7 @@ const dbhost =  process.env.DBHOST || "localhost";
 const dbport = process.env.DBPORT || "27017";
 const dbname = process.env.DBNAME || "autominder-app";
 
+
 debug("Starting connection to database...");
 
 // const dburi ="localhost:3500";  // USAR SOLAMENTE PARA TESTEAR LOCALMENTE LA CONEXION DE LA API CON EL LOCALHOST DE LA PC.
@@ -19,8 +20,8 @@ debug("Connection string received, trying connection...");
 const connect = async () => {
     
     try{
-        
-        await Mongoose.connect(dburi);  // Intento de conexion
+        Mongoose.set("strictQuery", false);
+        await Mongoose.connect(dburi), {useNewUrlParser: true};  // Intento de conexion
         debug(`Succesfuly connected to Mongo database!`); //Confirmación en consola de conexión
 
     } catch (error){
